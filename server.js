d453112935331a5f4d1fc10baff1c1eb1fd185c5
Vitaly <114258961+Erksery@users.noreply.github.com>
2024-12-23 15:40:19 +0300

    apidone

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const { server } = require("./scripts/serverCreation");
 const { getAnimals } = require("./services/getAnimals");
+const fs = require("fs");
+
+server.get("/getImage/:id", (req, res) => {
+  const id = req.params.id;
+  try {
+    fs.readFile(`images/${id}`, (err, data) => {
+      res.setHeader("Content-Type", "image/jpg");
+      res.send(data);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 server.get("/getAnimals", (req, res) => {
   getAnimals({ res });

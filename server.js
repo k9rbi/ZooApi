2894d50a2ba67a8c50aad6c8be1fb09143224560
k9rbi <sasha_ageev_00@inbox.ru>
2024-12-28 12:36:48 +0300

    post

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
+const multer = require("multer");
 const { server } = require("./scripts/serverCreation");
 const { getAnimals } = require("./services/getAnimals");
 const fs = require("fs");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images/");
+  },
+  filename: (req, file, cb) => {
+    /* file.originalname = Buffer.from(file.originalname, "latin1").toString(
+      "utf8"
+    );*/
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 server.get("/getImage/:id", (req, res) => {
   const id = req.params.id;
@@ -16,4 +31,10 @@ server.get("/getImage/:id", (req, res) => {
 
 server.get("/getAnimals", (req, res) => {
   getAnimals({ res });
+});
+
+server.post("/uploadImage", upload.single("file"), (req, res) => {
+  const file = req.file;
+  console.log(file);
+  res.status(200);
 });
--- services/getAnimals.js
+++ services/getAnimals.js
@@ -1,7 +1,7 @@
 const getAnimals = async ({ res }) => {
   try {
     const animalsData = await animals.find().toArray();
-    console.log(animalsData);
+
     res.status(200).json(animalsData);
   } catch (err) {
     console.log("Причина ошибки = " + err);
